ba5ea94196983cb08411e055fb00f8b7fc401e8b
TechBuilderTeam <saashid18@gmail.com>
2024-09-01 23:18:52 +0600

    Navbar done

--- src/components/Layouts/MainLayouts.tsx
+++ src/components/Layouts/MainLayouts.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div className="">
+    <div className="bg-black">
       <Navbar />
       <Outlet ></Outlet>
       <Footer></Footer>
