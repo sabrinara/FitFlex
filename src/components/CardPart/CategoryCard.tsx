85a667bd6307fb0a20df4be0cf7043eee74ea24a
TechBuilderTeam <saashid18@gmail.com>
2024-09-25 23:17:13 +0600

    home category item daynamic done

--- src/components/CardPart/CategoryCard.tsx
+++ src/components/CardPart/CategoryCard.tsx
@@ -1,9 +1,10 @@
+
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 // Define types for category items
 interface CategoryItem {
-  id: string;
+  _id: string;
   category: string;
 //   title2: string;
   image: string;
@@ -14,22 +15,26 @@ interface CategoryCardProps {
 }
 
 const CategoryCard = ({ category }: CategoryCardProps) => {
+  console.log(category);
+ 
   return (
     <div className="my-10 md:mb-8">
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 ">
         {category?.slice(8, 12)?.map((item: CategoryItem) => (
           <div
-            key={item.id}
+            key={item._id}
             className="flex flex-col justify-center items-center transform hover:scale-105 transition-transform duration-300"
           >
             <div className="px-3 bg-neutral-900 h-[15rem] rounded-full md:w-60 hover:bg-orange-800">
-              <div className="relative">
+             <Link to={`/products/${item._id}`}>
+             <div className="relative">
                 <img
                   src={item.image}
                   className="rounded-full h-[14rem] w-60"
                   alt={item.category}
                 />
               </div>
+             </Link>
             </div>
             <div className="pt-4 px-10 text-center">
               <h1 className="text-xl font-semibold text-orange-600">{item.category[2]}</h1>
